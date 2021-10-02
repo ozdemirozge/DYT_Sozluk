49d4e566a0b655d681e501c5659fdaaaf48407e8
Özge Özdemir <ozge.ozdemir@tccb.gov.tr>
2021-10-02 10:09:08 +0300

    ilk commit

---
+++ ios/Pods/Target Support Files/Pods-DYT_Sozluk-DYT_SozlukTests/Pods-DYT_Sozluk-DYT_SozlukTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_DYT_Sozluk_DYT_SozlukTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DYT_Sozluk_DYT_SozlukTestsVersionString[];
+
